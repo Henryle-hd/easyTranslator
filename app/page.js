4d8bf6d0b76f44e71e117ff1315ac3ac85c207fb
Henryle-hd <123258064+Henryle-hd@users.noreply.github.com>
2024-01-30 12:54:29 +0300

    nowCenter

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@ import AllWords from "./components/allWords"
 
 const page = () => {
   return (
-    <div className="flex flex-col justify-center ml-10 w-[40rem]" >
+    <div className="flex flex-col justify-center items-center mt-10 " >
       <h2 className="text-center">easyTranslator 🧑‍💻</h2>
 
-      <AllWords />
+      <div className="w-[40rem]">
+        <AllWords />
+      </div>
+      
       
     </div>
   )
