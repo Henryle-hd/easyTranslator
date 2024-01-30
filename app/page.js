5aa4b7e49c584cc91c1298a75a7e7006daca8b3b
Henryle-hd <123258064+Henryle-hd@users.noreply.github.com>
2024-01-30 12:40:52 +0300

    Tocenter

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import AllWords from "./components/allWords"
 
 const page = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center ml-10">
       <h2 className="text-center">easyTranslator 🧑‍💻</h2>
       <AllWords/>
     </div>
